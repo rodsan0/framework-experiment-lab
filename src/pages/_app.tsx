858d47a2e50f58d3951558ed54ce3cf6cd012a61
rodsan0 <rodsan0@users.noreply.github.com>
2023-04-11 00:42:28 -0400

    add roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto_Flex } from 'next/font/google';
+
+const roboto = Roboto_Flex({
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
